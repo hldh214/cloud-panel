bccb27f8c479acc8d17471503af3a662b6cdfe1c
Jim <hldh214@gmail.com>
2019-01-27 12:53:33 +0800

    add refresh proto

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,7 +2,7 @@ window.onload = function () {
     ws.send('refresh');
 };
 
-let ws = new WebSocket('ws://localhost:8888/ws');
+let ws = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port + '/ws');
 
 ws.onmessage = function (event) {
     console.log(event);
