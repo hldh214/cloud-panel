a300b17e74c52ca0894200a63ef5943918ab7b3e
Jim <hldh214@gmail.com>
2019-02-01 17:08:48 +0800

    refresh front-end

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -5,5 +5,18 @@ ws.onopen = function () {
 };
 
 ws.onmessage = function (event) {
-    console.log(event);
+    let data = JSON.parse(event.data);
+
+    switch (data.type) {
+        case 'refresh':
+            let tr = $('<tr></tr>');
+            data.nodes.forEach(function (each) {
+                $('<td></td>').text(each.uuid).appendTo(tr);
+                $('<td></td>').text(each.state).appendTo(tr);
+                $('<td></td>').text(each.public_ips).appendTo(tr);
+                $('<td></td>').text(each.provider_name).appendTo(tr);
+            });
+            $('#tbody').append(tr);
+            break;
+    }
 };
